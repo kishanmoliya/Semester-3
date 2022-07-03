671d27884965c39c78225e5795978f768e9b58d0
kishanmoliya <kishanmoliya232@gmail.com>
2022-07-03 19:23:06 +0530

    .

--- Data Structure/Lab-3/studentDemo.java
+++ Data Structure/Lab-3/studentDemo.java
@@ -1,35 +1,41 @@
 /* Create array of object of class Student_Detail with attributes Enrollment_no, Name, Sem, CPI for 5 
 students, scan their information and print it.(Home Work)  */
+import java.util.Scanner;
 class student_Details{
     long Enrollment_no;
     String name;
     int sem;
     double CPI;
+    Scanner sc = new Scanner(System.in);
 
-    public student_Details(long Enrollment_no, String name, int sem, double CPI){
-        this.Enrollment_no = Enrollment_no;
-        this.name = name;
-        this.sem = sem;
-        this.CPI = CPI;
+    public void getDetails(){
+       System.out.print("Enter Enroll_No = ");
+       Enrollment_no = sc.nextLong();
+
+       sc.nextLine();
+       System.out.print("Enter Name = ");
+       name = sc.nextLine();
+
+       System.out.print("Enter Sem = ");
+       sem = sc.nextInt();
+
+       System.out.print("Enter CPI = ");
+       CPI = sc.nextDouble();
     }
     public void printDetails(){
+        System.out.print("\n"+"~~~~~Student Details~~~~~");
         System.out.println("Enrollment = "+Enrollment_no);
         System.out.println("Name = "+name);
         System.out.println("Sem = "+sem);
-        System.out.println("CPI = "+CPI);
+        System.out.println("CPI = "+CPI + "\n");
     }
 }
 class studentDemo{
     public static void main(String[] args) {
         student_Details st[] = new student_Details[5];
-        st[0] = new student_Details(210101128,"Kishan",3,7.91);
-        st[1] = new student_Details(210101129,"Karan",3,7.00);
-        st[2] = new student_Details(210101130,"Uttam",3,7.56);
-        st[3] = new student_Details(210101131,"Rohan",3,7.48);
-        st[4] = new student_Details(210101132,"Yash",3,7.86);
-
-        for(int i=0;i<5;i++){
-            System.out.println("~~~~~~~~~~~~Students "+(i+1)+" Details ~~~~~~~~~~~");
+        for(int i=0; i<5;i++){
+            st[i] = new student_Details();
+            st[i].getDetails();
             st[i].printDetails();
         }
     }
