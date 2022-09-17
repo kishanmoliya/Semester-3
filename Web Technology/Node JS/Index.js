dc6f4d195e99a28ad283361469c7ab0f86a358bc
kishanmoliya <kishanmoliya232@gmail.com>
2022-09-17 21:51:31 +0530

    .

--- Web Technology/Node JS/Index.js
+++ Web Technology/Node JS/Index.js
@@ -1,1 +1,12 @@
-console.log("Node.js is an open source server environment");
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type' , 'text/html');
+    res.write("hello <h1> world </h1> from du!");
+    res.end();
+});
+
+server.listen(3000,()=>{
+    console.log("server started @ http://localhost:3000");
+});
